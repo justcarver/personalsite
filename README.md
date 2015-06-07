ecd2550b8649d94781a29f5648fa8a579be65303
Justin Carver <jccarver@gmail.com>
2015-06-07 03:52:23 -0400

    Set up system to build website using GulpJS to test out features

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# personalsite
-Personal Site
+# Justin Carver's Personal Site
+
+A test of a personal site project using gulp.js
--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,91 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+del = require('del')
+// var concat = require('gulp-concat');
+// var jshint = require('gulp-jshint');
 
-// Lint Task
-gulp.task('lint',function() {
-	return gulp.src('js/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+
+// Concatonate our Javascript Files
+gulp.task('scripts',function(){
+    gulp.src(['app/js/**/*.js','!app/js/**/*.min.js'])
+    .pipe(plumber())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(reload({stream:true}));
 });
 
-// Compile Our Sass
-gulp.task('sass', function() {
-	return gulp.src('scss/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('css'));
+// Sass
+gulp.task('sass',function(){
+    gulp.src('app/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({stream:true}));
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function () {
-	return gulp.src('js/*.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest(dist))
-		.pipe(rename('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+// HTML Task
+gulp.task('html',function(){
+    gulp.src('app/**/*.html')
+    .pipe(reload({stream:true}));
 });
 
-// Watch Files for Changes
-gulp.task('watch',function() {
-	gulp.watch('js/*.js', ['lint', 'scripts']);
-	gulp.watch('scss/*.scss', ['sass']);
+// ////////////////////////
+// Build Tasks
+
+// clean out all files and foulders from build folder
+gulp.task('build:cleanfolder',function(cb){
+    del([
+        'build/**'
+    ], cb);
+});
+// task to create built directory of all files
+gulp.task('build:copy',['build:cleanfolder'],function(){
+    return gulp.src('app/**/*/')
+    .pipe(gulp.dest('build/'))
+});
+// task to remove unwanted files
+// list all files and directories here that you don't want to include
+gulp.task('build:remove', ['build:copy',], function(cb){
+    del([
+        'build/scss/',
+        'build/js/!(*min.js)'
+    ], cb);
+});
+
+gulp.task('build', ['build:copy','build:remove']);
+// ////////////////////////
+
+//Browser Sync tasks
+gulp.task('browser-sync',function(){
+    browserSync({
+        server:{
+            baseDir: "./app/"
+        }
+    })
+});
+
+gulp.task('build:serve',function(){
+    browserSync({
+        server:{
+            baseDir: "./build/"
+        }
+    })
+});
+
+// Watch Task
+gulp.task('watch',function(){
+    gulp.watch('app/js/*.js', ['scripts']);
+    gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('app/scss/*.html', ['html']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task ('default',['scripts','sass','html','browser-sync','watch']);
